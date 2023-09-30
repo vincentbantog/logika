27971affc013313f259f599cec3f69e2fad37fa1
Vincent <vincentbantog12@gmail.com>
2023-10-01 00:13:22 +0800

    BotNavViewer Commit 4 - HomeFragment shows first

--- app/src/main/java/com/example/logika/MainMenu.java
+++ app/src/main/java/com/example/logika/MainMenu.java
@@ -6,13 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainMenu extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    StatsFragment statsFragment = new StatsFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        showBottomNavigationDrawer();
+    }
+
+    public void showBottomNavigationDrawer() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainMenuContainer,homeFragment).commit();
+
 
 
     }
