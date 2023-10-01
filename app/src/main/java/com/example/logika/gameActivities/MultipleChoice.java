d7a7705749467e10227f3500e3e936b1045144f6
Vincent <vincentbantog12@gmail.com>
2023-10-01 15:34:35 +0800

    GameSelectionScreen Commit 3
    Failed attempt to add back button logic to Multiple Choice game

--- app/src/main/java/com/example/logika/gameActivities/MultipleChoice.java
+++ app/src/main/java/com/example/logika/gameActivities/MultipleChoice.java
@@ -1,6 +1,8 @@
 package com.example.logika.gameActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,14 +14,38 @@ import com.example.logika.mainMenuFragments.HomeFragment;
 
 public class MultipleChoice extends AppCompatActivity {
 
-
+    private Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiple_choice);
 
+        configureBtnBack();
+
 
     }
 
+    public void configureBtnBack(){
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateBackToHome();
+            }
+        });
+    }
 
+    public void navigateBackToHome(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        boolean isFragmentInBackStack = fragmentManager.popBackStackImmediate("homeFragmentTag", 0);
+
+        if (!isFragmentInBackStack) {
+            Fragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.mainMenuContainer, homeFragment, "homeFragmentTag")
+                    .addToBackStack("homeFragmentTag")
+                    .commit();
+        }
+    }
 }
