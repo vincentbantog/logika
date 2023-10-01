bfb297e6c7b25f3435a03f8829746c7be861817d
Vincent <vincentbantog12@gmail.com>
2023-10-01 16:38:27 +0800

    TrueOrFalseNavigation Commit 1
    Testing the removal of unnecessary code in fragment

---
+++ app/src/main/java/com/example/logika/gameActivities/TrueOrFalse.java
@@ -0,0 +1,16 @@
+package com.example.logika.gameActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.logika.R;
+
+public class TrueOrFalse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_true_or_false);
+    }
+}
