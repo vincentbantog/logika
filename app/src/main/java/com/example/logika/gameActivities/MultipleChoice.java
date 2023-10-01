1c107af87144de70681fac7d7f3af99e1a965c8a
Vincent <vincentbantog12@gmail.com>
2023-10-01 15:47:44 +0800

    GameSelectionScreen Commit 4
    Added a Back Button to the MultipleChoice game

--- app/src/main/java/com/example/logika/gameActivities/MultipleChoice.java
+++ app/src/main/java/com/example/logika/gameActivities/MultipleChoice.java
@@ -7,18 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.logika.MainMenu;
 import com.example.logika.R;
 import com.example.logika.mainMenuFragments.HomeFragment;
 
 public class MultipleChoice extends AppCompatActivity {
 
-
+    private Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiple_choice);
 
+        configureBackButton();
 
+
+    }
+
+    public void configureBackButton(){
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(MultipleChoice.this, MainMenu.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        });
     }
 
 
