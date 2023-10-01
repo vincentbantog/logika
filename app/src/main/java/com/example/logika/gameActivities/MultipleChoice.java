48feece88162e12a3f4fa0d49257668fe7c5ad5d
Vincent <vincentbantog12@gmail.com>
2023-10-01 15:20:31 +0800

    GameSelectionScreen Commit 1
    Added four buttons for game selection screen
    Added an activity for Multiple Choice

---
+++ app/src/main/java/com/example/logika/gameActivities/MultipleChoice.java
@@ -0,0 +1,25 @@
+package com.example.logika.gameActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.logika.R;
+import com.example.logika.mainMenuFragments.HomeFragment;
+
+public class MultipleChoice extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiple_choice);
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/logika/mainMenuFragments/HomeFragment.java
+++ app/src/main/java/com/example/logika/mainMenuFragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.logika.mainMenuFragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.logika.R;
+import com.example.logika.gameActivities.MultipleChoice;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +29,9 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button btnMultipleChoice;
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -61,6 +68,24 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        configureBtnMultipleChoice(view);
+
+
+        return view;
     }
+
+    public void configureBtnMultipleChoice(View view){
+        btnMultipleChoice = view.findViewById(R.id.btnMultipleChoice);
+        btnMultipleChoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(getActivity(), MultipleChoice.class);
+                startActivity(mainIntent);
+            }
+        });
+    }
+
+
 }
