2596bda24d891a50fe5ba37b79e82029fc093706
Vincent <vincentbantog12@gmail.com>
2023-10-03 00:59:20 +0800

    LogicSim Commit 6
    Defined and Initialized the view elements in the Quiz Activity
    Create the Database and read it on an DB Browser for SQLite

--- app/src/main/java/com/example/logika/gameActivities/logicSim/RadioButtonChoices.java
+++ app/src/main/java/com/example/logika/gameActivities/logicSim/RadioButtonChoices.java
@@ -6,12 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.example.logika.R;
+
+import java.util.List;
 
 public class RadioButtonChoices extends AppCompatActivity {
 
     private Button btnBack;
+    private TextView textViewQuestion;
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
+    private TextView textViewCountDown;
+    private RadioGroup rbGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private Button buttonConfirmNext;
+
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,11 @@ public class RadioButtonChoices extends AppCompatActivity {
         setContentView(R.layout.activity_radio_button_choices);
 
         configureBackButton();
+        initializeViewElements();
+
+        QuizDbHelper dbHelper = new QuizDbHelper(this);
+        questionList = dbHelper.getAllQuestion();
+
     }
 
     public void configureBackButton () {
@@ -32,4 +53,17 @@ public class RadioButtonChoices extends AppCompatActivity {
             }
         });
     }
+
+    public void initializeViewElements() {
+        textViewQuestion = findViewById(R.id.txtQuestion);
+        textViewScore = findViewById(R.id.txtScore);
+        textViewQuestionCount = findViewById(R.id.txtQuestionNumber);
+        textViewCountDown = findViewById(R.id.txtTimer);
+        rbGroup = findViewById(R.id.radio_group);
+        rb1 = findViewById(R.id.radio_button_1);
+        rb2 = findViewById(R.id.radio_button_2);
+        rb3 = findViewById(R.id.radio_button_3);
+        buttonConfirmNext = findViewById(R.id.btnConfirmNext);
+    }
+
 }
