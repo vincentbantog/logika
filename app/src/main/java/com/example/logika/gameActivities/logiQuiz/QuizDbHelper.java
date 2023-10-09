5c8904d88641699bf5718cbba627a2db6c65e8d0
Vincent <vincentbantog12@gmail.com>
2023-10-09 23:19:26 +0800

    LogiQuiz Commit 2
    Created the helper class

---
+++ app/src/main/java/com/example/logika/gameActivities/logiQuiz/QuizDbHelper.java
@@ -0,0 +1,65 @@
+package com.example.logika.gameActivities.logiQuiz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.logika.gameActivities.logiQuiz.QuizContract.*;
+
+import androidx.annotation.Nullable;
+
+public class QuizDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "MyAwesomeQuiz.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
+
+
+    public QuizDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionTable.TABLE_NAME + " ( " +
+                QuestionTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionTable.COLUMN_QUESTION + " INTEGER, " +
+                QuestionTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionTable.COLUMN_ANSWER_NR + " INTEGER" +
+                ")";
+
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionsTable();
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + QuestionTable.TABLE_NAME);
+        onCreate(db);
+    }
+
+    private void fillQuestionsTable() {
+        Question q1 = new Question(1,  "A", "B", "C", 1);
+        addQuestion(q1);
+
+    }
+
+    private void addQuestion(Question question) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionTable.COLUMN_QUESTION, question.getCircuitFragment());
+        cv.put(QuestionTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuestionTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuestionTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuestionTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        db.insert(QuestionTable.TABLE_NAME, null, cv);
+
+    }
+}
