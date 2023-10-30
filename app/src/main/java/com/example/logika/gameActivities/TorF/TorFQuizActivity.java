e18b56ddc9281a24ee75594658677c461ee224f8
Vincent <vincentbantog12@gmail.com>
2023-10-31 02:43:25 +0800

    T_or_F Commit 5
    Added a basic timer progress bar

--- app/src/main/java/com/example/logika/gameActivities/TorF/TorFQuizActivity.java
+++ app/src/main/java/com/example/logika/gameActivities/TorF/TorFQuizActivity.java
@@ -2,25 +2,35 @@ package com.example.logika.gameActivities.TorF;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.logika.R;
+import com.example.logika.gameActivities.logiQuiz.LogicGate;
+import com.example.logika.gameActivities.logiQuiz.QuizActivity;
 
 import java.util.Collections;
 import java.util.List;
 
 public class TorFQuizActivity extends AppCompatActivity {
+    private static final long COUNTDOWN_IN_MILLIS = 30000;
 
     private TextView txtScore;
     private TextView txtLives;
+
+    private ProgressBar timerBar;
+
     private TextView txtQuestionCount;
     private TextView txtQuestion;
 
@@ -33,6 +43,10 @@ public class TorFQuizActivity extends AppCompatActivity {
     private ColorStateList textColorDefaultRb;
 
     private List<TFQuestion> questionList;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
+
     private int questionCounter;
     private int questionCountTotal;
     private TFQuestion currentQuestion;
@@ -43,6 +57,8 @@ public class TorFQuizActivity extends AppCompatActivity {
 
     private int livesCount;
     private boolean isGameOver;
+
+    private long backPressedTime;
 
 
     @Override
@@ -82,6 +98,7 @@ public class TorFQuizActivity extends AppCompatActivity {
 
     private void initializeViewElements(){
         txtScore = findViewById(R.id.txtScore);
+        timerBar = findViewById(R.id.timerBar);
         txtLives = findViewById(R.id.txtLives);
         txtQuestionCount = findViewById(R.id.txtQuestionCount);
         txtQuestion = findViewById(R.id.txtQuestion);
@@ -113,9 +130,34 @@ public class TorFQuizActivity extends AppCompatActivity {
             txtQuestionCount.setText("0" + questionCounter + "/" + questionCountTotal);
             answered = false;
             btnConfirm.setText("Confirm");
+
+            timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountdown();
         } else {
          finishQuiz();
         }
+    }
+
+    private void startCountdown() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateProgressBar();
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeftInMillis = 0;
+                timerBar.setProgress(0);
+                checkAnswer();
+            }
+        }.start();
+    }
+
+    private void updateProgressBar(){
+        int progress = (int) (timeLeftInMillis * 100 / COUNTDOWN_IN_MILLIS);
+        timerBar.setProgress(progress);
     }
 
     private void checkAnswer(){
@@ -157,5 +199,26 @@ public class TorFQuizActivity extends AppCompatActivity {
 
     private void finishQuiz(){
         finish();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            Intent intent = new Intent(TorFQuizActivity.this, TrueOrFalse.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Press back again to confirm", Toast.LENGTH_SHORT).show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null){
+            countDownTimer.cancel();
+        }
     }
 }
