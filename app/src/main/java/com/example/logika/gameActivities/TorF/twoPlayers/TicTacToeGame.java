62c8c36431cef82e7faf1efe49d935dfa640b14c
Vincent <vincentbantog12@gmail.com>
2023-10-31 16:54:01 +0800

    T_or_F Commit 8
    - Created the blank layout for the tic-tac-toe game

---
+++ app/src/main/java/com/example/logika/gameActivities/TorF/twoPlayers/TicTacToeGame.java
@@ -0,0 +1,16 @@
+package com.example.logika.gameActivities.TorF.twoPlayers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.logika.R;
+
+public class TicTacToeGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tic_tac_toe_game);
+    }
+}
--- app/src/main/java/com/example/logika/gameActivities/TorF/twoPlayers/TicTacToeMain.java
+++ app/src/main/java/com/example/logika/gameActivities/TorF/twoPlayers/TicTacToeMain.java
@@ -2,15 +2,30 @@ package com.example.logika.gameActivities.TorF.twoPlayers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.logika.R;
 
 public class TicTacToeMain extends AppCompatActivity {
 
+    private Button btnBegin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe_main);
+
+        btnBegin = findViewById(R.id.btnBegin);
+        btnBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TicTacToeMain.this, TicTacToeGame.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
