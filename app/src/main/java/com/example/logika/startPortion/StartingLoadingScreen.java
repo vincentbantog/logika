f56bdc19bf36d9508bc7928fa29312f2fab6355e
Vincent <vincentbantog12@gmail.com>
2023-11-20 01:52:06 +0800

    start_portion_design Commit 1
    Starting splash screen
    - Implemented the mp4 video for the splash screen

--- app/src/main/java/com/example/logika/startPortion/StartingLoadingScreen.java
+++ app/src/main/java/com/example/logika/startPortion/StartingLoadingScreen.java
@@ -3,20 +3,53 @@ package com.example.logika.startPortion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 import com.example.logika.R;
 
 public class StartingLoadingScreen extends AppCompatActivity {
+
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_loading_screen);
 
-        showStartingLoadingScreen();
+        videoView = findViewById(R.id.videoView);
+
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.l_load1;
+
+        Uri videoUri = Uri.parse(videoPath);
+
+        videoView.setVideoURI(videoUri);
+
+        videoView.start();
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Intent intent = new Intent(StartingLoadingScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
+
+
+
     public void showStartingLoadingScreen(){
         new Handler().postDelayed(new Runnable() {
             @Override
