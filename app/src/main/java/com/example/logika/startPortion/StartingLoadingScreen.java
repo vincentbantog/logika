b72bb830a37827c949a77cdd4920692bfdfdf5ac
Vincent <vincentbantog12@gmail.com>
2023-12-02 18:04:35 +0800

    start_screen_optimization Commit 1
    - Fitted the video to be in the middle of the screen
    - Added a background image in case the video does not fit

--- app/src/main/java/com/example/logika/startPortion/StartingLoadingScreen.java
+++ app/src/main/java/com/example/logika/startPortion/StartingLoadingScreen.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -22,8 +23,7 @@ public class StartingLoadingScreen extends AppCompatActivity {
 
 
     private VideoView videoView;
-    private ImageView imageView;
-    private Button btnStart;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,103 +31,29 @@ public class StartingLoadingScreen extends AppCompatActivity {
         setContentView(R.layout.activity_starting_loading_screen);
 
         videoView = findViewById(R.id.videoView);
-        imageView = findViewById(R.id.imageView);
-        btnStart = findViewById(R.id.btnStart);
 
-        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.l_load1;
+
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.l_load2;
 
         Uri videoUri = Uri.parse(videoPath);
 
         videoView.setVideoURI(videoUri);
 
         // Adjust the video view to cover the entire screen
-//        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-//            @Override
-//            public void onPrepared(MediaPlayer mediaPlayer) {
-//                mediaPlayer.setLooping(false); // Set looping true/false as per your requirement
-//
-//                // Adjust the size of the video view to cover the entire screen
-//                int videoWidth = mediaPlayer.getVideoWidth();
-//                int videoHeight = mediaPlayer.getVideoHeight();
-//
-//                float videoProportion = (float) videoWidth / (float) videoHeight;
-//                int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
-//                int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
-//                float screenProportion = (float) screenWidth / (float) screenHeight;
-//
-//                android.view.ViewGroup.LayoutParams lp = videoView.getLayoutParams();
-//
-//                if (videoProportion > screenProportion) {
-//                    lp.width = screenWidth;
-//                    lp.height = (int) ((float) screenWidth / videoProportion);
-//                } else {
-//                    lp.height = screenHeight;
-//                    lp.width = (int) ((float) screenHeight * videoProportion);
-//                }
-//
-//                videoView.setLayoutParams(lp);
-//            }
-//        });
-
-
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(false);
+            }
+        });
 
         videoView.start();
 
         videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
-                showImageView();
-                showButton();
-                configureStartButton();
-            }
-        });
-
-    }
-
-    public void showImageView(){
-//        adjustImageViewSize(imageView);
-        RelativeLayout relLayoutImage = findViewById(R.id.relLayoutImage);
-        relLayoutImage.setVisibility(View.VISIBLE);
-    }
-
-    private void adjustImageViewSize(ImageView imageView) {
-        imageView.post(new Runnable() {
-            @Override
-            public void run() {
-                int screenWidth = getResources().getDisplayMetrics().widthPixels;
-                int screenHeight = getResources().getDisplayMetrics().heightPixels;
-
-                int imageWidth = imageView.getDrawable().getIntrinsicWidth();
-                int imageHeight = imageView.getDrawable().getIntrinsicHeight();
-
-                float widthRatio = (float) screenWidth / imageWidth;
-                float heightRatio = (float) screenHeight / imageHeight;
-
-                float scaleFactor = Math.min(widthRatio, heightRatio);
-
-                int finalWidth = (int) (imageWidth * scaleFactor);
-                int finalHeight = (int) (imageHeight * scaleFactor);
-
-                imageView.getLayoutParams().width = finalWidth;
-                imageView.getLayoutParams().height = finalHeight;
-                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-                imageView.requestLayout();
-            }
-        });
-    }
-
-    private void showButton() {
-        RelativeLayout wholeLayout = findViewById(R.id.relLayoutStart);
-
-        wholeLayout.setVisibility(View.VISIBLE);
-    }
-
-    public void configureStartButton() {
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mainIntent = new Intent(StartingLoadingScreen.this, SecondLoadingScreen.class);
-                startActivity(mainIntent);
+                Intent intent = new Intent(StartingLoadingScreen.this, MainActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
