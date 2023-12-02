19bc6a5b1ceff8ccc218f8fb22c2a0bd53c4a657
Vincent <vincentbantog12@gmail.com>
2023-12-02 22:02:20 +0800

    start_screen_optimization Commit 2
    - Added a fade out effect after the starting logo screen
    - Centered the second loading screen to the middle

--- app/src/main/java/com/example/logika/startPortion/StartingLoadingScreen.java
+++ app/src/main/java/com/example/logika/startPortion/StartingLoadingScreen.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -23,6 +25,8 @@ public class StartingLoadingScreen extends AppCompatActivity {
 
 
     private VideoView videoView;
+    private ImageView backgroundImage;
+
 
 
     @Override
@@ -31,30 +35,58 @@ public class StartingLoadingScreen extends AppCompatActivity {
         setContentView(R.layout.activity_starting_loading_screen);
 
         videoView = findViewById(R.id.videoView);
+        backgroundImage = findViewById(R.id.background_image);
 
 
+        videoView.setVisibility(View.VISIBLE);
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.l_load2;
-
         Uri videoUri = Uri.parse(videoPath);
-
         videoView.setVideoURI(videoUri);
 
-        // Adjust the video view to cover the entire screen
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.setLooping(false);
+            public void onCompletion(MediaPlayer mp) {
+                startFadeInAnimation(backgroundImage);
+//                Intent intent = new Intent(StartingLoadingScreen.this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
             }
         });
 
         videoView.start();
 
-        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+
+    }
+
+    private void startFadeInAnimation(ImageView imageView) {
+        imageView.setVisibility(View.VISIBLE);
+        AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeIn.setDuration(3000); // Set your desired duration here
+        imageView.startAnimation(fadeIn);
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onCompletion(MediaPlayer mp) {
-                Intent intent = new Intent(StartingLoadingScreen.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(StartingLoadingScreen.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 1000);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
             }
         });
     }
