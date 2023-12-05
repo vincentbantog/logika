9d79d23391bcb92d89a29480c2a2c8ea11bda700
Vincent <vincentbantog12@gmail.com>
2023-12-05 14:38:32 +0800

    removal_of_action_bar Commit 3
    - removed the action bar of:
    - T or F

--- app/src/main/java/com/example/logika/HomeFragment/gameActivities/TorF/singlePlayer/InstructionsActivities/TorFInstructions1.java
+++ app/src/main/java/com/example/logika/HomeFragment/gameActivities/TorF/singlePlayer/InstructionsActivities/TorFInstructions1.java
@@ -1,10 +1,13 @@
 package com.example.logika.HomeFragment.gameActivities.TorF.singlePlayer.InstructionsActivities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.logika.HomeFragment.gameActivities.TorF.singlePlayer.TrueOrFalse;
@@ -17,6 +20,12 @@ public class TorFInstructions1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
         setContentView(R.layout.activity_tor_finstructions1);
 
         btnBack = findViewById(R.id.btnBack);
--- app/src/main/java/com/example/logika/HomeFragment/gameActivities/TorF/singlePlayer/TorFEndActivity.java
+++ app/src/main/java/com/example/logika/HomeFragment/gameActivities/TorF/singlePlayer/TorFEndActivity.java
@@ -1,10 +1,13 @@
 package com.example.logika.HomeFragment.gameActivities.TorF.singlePlayer;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -29,6 +32,12 @@ public class TorFEndActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
         setContentView(R.layout.activity_tor_fend);
 
         txtScore = findViewById(R.id.txtScore);
